72f7ad97eb8cd37b9f1f5d03d9d6397ff381e0c7
ilyos nishanov <ilyosnishanov@ilyoss-MacBook-Air.local>
2024-10-03 10:52:44 +0500

    fib and fac
     Changes to be committed:
    	modified:   bb.ipynb
    	modified:   factorial.py
    	modified:   fibonacci.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,7 @@
-# import sys
+import sys
+from time import time
 
+#with list
 def factorial1(n):
     n_int = int(n)
     result = 1
@@ -8,7 +10,7 @@ def factorial1(n):
     return result
 
 
-
+#with recursive function
 def factorial2(n):
     if n == 1:
         return 1
@@ -16,11 +18,24 @@ def factorial2(n):
         return n*factorial2(n-1)
 
 
-from time import time
+#with memoization
+def factorial3(n,memo={}):
+    if n in memo:
+        return memo[n]
+    if n == 1:
+        return 1
+    else:
+        memo[n]= n*factorial3(n-1)
+        return memo[n]
+
+
 t0=time()
-factorial2(900)
+print(factorial1(900))
 t1=time()
-factorial1(900)    #print(factorial(sys.argv[1]))
+print(factorial2(900))    #print(factorial(sys.argv[1]))
 t2=time()
+print(factorial3(900))    #print(factorial(sys.argv[1]))
+t3=time()
 print ('function vers1 takes %f' %(t1-t0))
 print ('function vers2 takes %f' %(t2-t1))
+print ('function vers3 takes %f' %(t3-t2))
--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,32 @@
 import sys
-def fib(n):
+from time import time
+
+#brute force
+def fib_bf(n):
     n_int = int(n)
     if n_int ==0 :
         return 0
     elif n_int ==1:
         return 1
     else:
-        return fib(n_int-1)+fib(n_int-2)
+        return fib_bf(n_int-1)+fib_bf(n_int-2)
     
-print(fib(sys.argv[1]))
+
+
+#with memoization
+def fib_wm(n, memo ={}):
+    n_int = int(n)
+    if n_int in memo:
+        return memo[n_int]
+    if n_int <=2:
+        return 1
+    memo[n_int]= fib_wm(n_int-1,memo)+fib_wm(n_int-2,memo)
+    return memo[n_int]
+
+# t0=time()
+# print(fib_bf(sys.argv[1]))
+t1=time()
+print(fib_wm(sys.argv[1]))
+t2=time()
+# print ('function vers1 takes %f' %(t1-t0))
+print ('function vers2 takes %f' %(t2-t1))
